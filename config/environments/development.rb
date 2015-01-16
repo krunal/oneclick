d7bacfe718e1df4a8b982d37f29f7adf76b373d4
Krunal <krunal.shah@hotmail.com>
2015-01-15 19:51:39 -0600

    Initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,15 +39,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'localhost:3000',
-    user_name:            "krumasta@gmail.com",
-    password:             "bnhh78978095",
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-
+ 
   config.action_mailer.perform_deliveries = true
 end
